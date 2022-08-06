ec14ee50f6b846955bf50d0a67a35b45374ca8bd
akashj6996 <akashramujadhav@gmail.com>
2022-08-06 12:49:13 +0530

    4th commit

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,10 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   exports: [UserDashboardComponent],
   declarations: [UserDashboardComponent],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule
+    
+  ],
 })
 export class UserModule {}
